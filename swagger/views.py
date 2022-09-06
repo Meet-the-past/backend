917db4ad602da7f24fcdd8f3150b362a968b9050
aristo <dkfud2121@naver.com>
2022-09-06 23:24:04 +0900

    refactor : test swagger code complete

--- swagger/views.py
+++ swagger/views.py
@@ -7,15 +7,30 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .open_api_params import get_params, post_params
+from .serializer import GetRequestSerializer, GetResponseSerializer, PostRequestSerializer, PostResponseSerializer
 
 
 class View(APIView):
     permission_classes = [permissions.AllowAny]
 
     @swagger_auto_schema(manual_parameters=get_params)
-    def get(selfself, request):
+    def get(self, request):
         return Response("Swagger 연동 테스트")
 
     @swagger_auto_schema(request_body=post_params)
+    def post(self, request):
+        return Response("Swagger Schema")
+
+class SerializerView(APIView):
+    permission_classes = [permissions.AllowAny]
+    @swagger_auto_schema(query_serializer=GetRequestSerializer, responses={"200":GetResponseSerializer})
+    def get(self, request):
+        return Response("Swagger 연동 테스트")
+
+    @swagger_auto_schema()
+    def post(self, request):
+        return Response("Swagger Schema")
+
+    @swagger_auto_schema(request_body=PostRequestSerializer, responses={"201": PostResponseSerializer})
     def post(self, request):
         return Response("Swagger Schema")

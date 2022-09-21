2050e86e22e2f258e7f4c7ebd35ff6982631241a
teawon <xodnjsvkvk9@gmail.com>
2022-09-21 11:58:26 +0900

    chore : import ai_processModule

--- images/tasks.py
+++ images/tasks.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import, unicode_literals
+from email.mime import image
 from celery import shared_task
 
 from asyncio import sleep
-
-
 from backend.celery import app
+# import sys
+# sys.path.append( "/backend/ai/photo_restoration/processAI")
+from ai.photo_restoration.processAI import ai_process
+
 
 
 
@@ -26,9 +29,15 @@ def add(x, y):
 #from .views import get_img_url
 @app.task
 def ai_task(request):
-    image_url = "향후 함수 추가 필요"
+    image_url = 0
     # result = get_ai_result(image_url)
-    sleep(10)
+
     #ai 처리
+    # for i in range(999999):
+    #     sleep(10)
+    #     print("잠자는중")
+    #     image_url = image_url+1
+    ai_process("uuid","imgPath")
+   
     return image_url
 
